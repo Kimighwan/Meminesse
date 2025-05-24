21e4a07c37f93d9121da86d2cd0f03d5b95d768b
조수민 <josoomin6954@naver.com>
2025-05-24 23:42:39 +0900

    설정 버튼 선택시 설정팝업 뜸 (+조금 오류)
    - 설정창 뜬 후 마우스로 선택 해줘야 키보드로도 선택할 수 있게되는 (방향키+엔터) 오류
    - 그래픽, 오디오 창 선택시 background( 주변이 검게 되는) 가 창 위로 올라와 어둡게 되는 오류     -> 분명 background가 더 밑에 레이어에 있는데 왜 이런지 다시 알아보겠음.

--- Assets/Scripts/UI/SettingsPopup.cs
+++ Assets/Scripts/UI/SettingsPopup.cs
@@ -1,19 +1,50 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class SettingsPopup : MonoBehaviour
 {
-    public GameObject _popup;
+    public GameObject firstButton;
+
+    public GameObject audioPopup;
+    public GameObject graphicPopup;
+    public GameObject controlPopup;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //EventSystem.current.SetSelectedGameObject(firstButton);   // Default -> first button is selected (audio button)
+        audioPopup.SetActive(true);          //audio tab active, others inactive
+        graphicPopup.SetActive(false);
+        controlPopup.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _popup.SetActive(false); // 팝업은 처음에 꺼져있어야함
+        //_popup.SetActive(false); // 팝업은 처음에 꺼져있어야함
         DontDestroyOnLoad(this); // 씬이 넘어가도 이 스크립트는 파괴되면 안됨
     }
+
+
+    public void OnClickAudioButton()
+    {
+        audioPopup.SetActive(true);          //audio tab active, others inactive
+        graphicPopup.SetActive(false);
+        controlPopup.SetActive(false);
+    }
+
+    public void OnClickGraphicButton()
+    {
+        graphicPopup.SetActive(true);       //graphic tab active, others inactive
+        audioPopup.SetActive(false);
+        controlPopup.SetActive(false);
+    }
+
+    public void OnClickControlButton()
+    {
+        controlPopup.SetActive(true);       //control tab active, others inactive
+        graphicPopup.SetActive(false);
+        audioPopup.SetActive(false);
+    }
+
 }
