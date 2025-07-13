519fab93b441e3d8daa379b256b74cd0066adde0
조수민 <josoomin6954@naver.com>
2025-07-13 14:26:18 +0900

    오류 해결
    UIBase의 메소드를 protected virtual로 바꾸고 MainMenu에서 오버라이딩

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,7 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : UIBase
 {
-    
+    protected override void Start()
+    {
+        base.Start();
+
+        // 커서 기본 설정
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+        // 포커스를 명시적으로 줘야 UI가 키보드 입력을 받음
+        EventSystem.current.SetSelectedGameObject(firstButton);
+
+        // 또한 currentButton도 초기화 필요
+        SetCurrentButton(firstButton);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        // UIBase에서 키보드 모드와 마우스 모드를 전환하는 로직이 있으므로, 여기서는 추가적인 로직이 필요하지 않음
+    }
 
     public void OnClickNewGame()
     {
--- Assets/Scripts/UI/UIBase.cs
+++ Assets/Scripts/UI/UIBase.cs
@@ -9,20 +9,22 @@ public class UIBase : MonoBehaviour
     private GameObject currentButton;
 
 
-    void Start()
+    protected virtual void Start()
     {
         isKeyboardMode = false;
         Cursor.visible = true;   // Default -> mouse cursor visible
         Cursor.lockState = CursorLockMode.None;
 
-        //EventSystem.current.SetSelectedGameObject(firstButton);   // Default -> first button is selected
+        // 포커스
+        EventSystem.current.SetSelectedGameObject(firstButton);
+        SetCurrentButton(firstButton);
     }
 
 
-    void Update()
+    protected virtual void Update()
     {
 
-        //  마우스 움직임 감지 → 마우스 모드로 전환
+        // 마우스 움직임 감지 → 마우스 모드로 전환
         if ((isKeyboardMode == true) && (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0))    // 조건 : 지금 키보드모드일 때 마우스 움직임 감지되면
         {
             Debug.Log("마우스 모드 진입");
@@ -33,11 +35,18 @@ public class UIBase : MonoBehaviour
         }
 
         // 처음에는 마우스모드로 시작하는데 그 조건으로 인하여 이 조건문으로 들어와서 맨 처음 버튼이 미리 선택되어있는 오류가 발생함    -> 지금은 아님 ???
-        //  키보드 누름 감지 → 키보드 모드로 전환
+        // 키보드 누름 감지 → 키보드 모드로 전환
         if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow)) && (isKeyboardMode == false))   //조건 : 지금 마우스모드일 때 키보드 방향키 눌리면 
         {
             Debug.Log("키보드 모드 진입");
-            EventSystem.current.SetSelectedGameObject(currentButton); //다시 선택
+
+            //EventSystem.current.SetSelectedGameObject(currentButton); //다시 선택
+            
+            if (currentButton != null)
+                EventSystem.current.SetSelectedGameObject(currentButton);
+            else
+                EventSystem.current.SetSelectedGameObject(firstButton);
+           
             isKeyboardMode = true;
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;  // Locked은 마우스 잠금용이라 None으로 유지하는 게 UI엔 더 낫다
