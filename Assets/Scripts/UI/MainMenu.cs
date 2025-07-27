56db85ebe6d7aedd2d2b46528df5dac53b5cd188
조수민 <josoomin6954@naver.com>
2025-07-28 01:22:29 +0900

    종료 확인창 구현 + 아트 자료 넣음
    종료 확인 창 왜 마우스 갖다댔을 때 색이 안바뀌는지 잘 모르겠음 그리고 키보드 작동 안됨
    
    Aseprite로 그린 자료를 Resources에 추가하였음

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -7,6 +7,9 @@ public class MainMenu : UIBase
 {
     [SerializeField]
     private GameObject settingUI;
+    //[SerializeField]
+    //private GameObject exitConfirmationUI;
+    public GameObject exitConfirmationPopup;
 
     protected override void Start()
     {
@@ -26,6 +29,7 @@ public class MainMenu : UIBase
     protected override void Update()
     {
         if (settingUI.activeSelf) return;
+        if (exitConfirmationPopup.activeSelf) return;
         base.Update();
         // UIBase에서 키보드 모드와 마우스 모드를 전환하는 로직이 있으므로, 여기서는 추가적인 로직이 필요하지 않음
     }
@@ -46,12 +50,28 @@ public class MainMenu : UIBase
 
     public void OnClickQuit()     //Quit button
     {
+        // 종료 재확인 팝업 창
+        exitConfirmationPopup.SetActive(true);
+        SetCurrentButton(exitConfirmationPopup.transform.Find("Yes").gameObject); // 팝업의 Yes 버튼에 포커스 설정
+
+    }
+
+    // 종료하시겠습니까 - 예
+    public void OnConfirmQuit()
+    {
         Debug.Log("Game closed");
 #if UNITY_EDITOR                  //Unity 에디터에서 실행 중일 때만 아래 코드를 실행
         UnityEditor.EditorApplication.isPlaying = false;       //에디터에서 Play 중지
 #else
         Application.Quit();       //실제 게임에서 종료
 #endif
+    }
+
+    // 종료하시겠습니까 - 아니오
+    public void OnCancelQuit()
+    {
+        // 팝업 닫기
+        exitConfirmationPopup.SetActive(false);
     }
 
 
