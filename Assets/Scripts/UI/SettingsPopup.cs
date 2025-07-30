af0b0a6fa425bff473541a84b619576155c6ad62
조수민 <josoomin6954@naver.com>
2025-07-30 23:07:48 +0900

    상위 버튼들과 하위 항목들 구분
    - esc 눌러서 상위 버튼으로 돌아가기
    - 상위버튼 선택 중일 때만 esc누르면 설정창 꺼짐
    - 선택된 상위버튼은 흰색으로 바뀜 (하위 항목 선택 중일 때에도)

--- Assets/Scripts/UI/SettingsPopup.cs
+++ Assets/Scripts/UI/SettingsPopup.cs
@@ -12,6 +12,17 @@ public class SettingsPopup : UIBase
     public GameObject graphicPopup;
     public GameObject controlPopup;
 
+    // 포커스가 상위부분에 있는지 하위부분에 있는지 판단하는 변수
+    private static bool isTopFocus = true;
+
+    [SerializeField]
+    public TextMeshProUGUI currentBtnText;
+    [SerializeField] 
+    private TextMeshProUGUI[] topButtonTexts;
+
+    private Color32 defaultColor = new Color32(170, 131, 167, 255);
+    private Color32 selectedColor = Color.white;
+
     //오디오 슬라이더 관리
     [SerializeField]
     private Slider BGMslider;
@@ -32,7 +43,6 @@ public class SettingsPopup : UIBase
         base.Start(); //UIBase꺼 그대로 사용 
         currentButton = invisibleDummyButton; // 현재 선택된 버튼을 더미버튼으로 초기화
 
-        //EventSystem.current.SetSelectedGameObject(firstButton);   // Default -> first button is selected (audio button)
         audioPopup.SetActive(true);          //audio tab active, others inactive
         graphicPopup.SetActive(false);
         controlPopup.SetActive(false);
@@ -46,28 +56,60 @@ public class SettingsPopup : UIBase
     protected override void Update()
     {
         base.Update();
-        // esc버튼 - 종료
-        if (Input.GetKeyDown(KeyCode.Escape))
+
+        ChangeTextColor();
+        if (isTopFocus)
         {
-            Debug.Log("설정 팝업 닫힘");
-            this.popUp.SetActive(false); // 팝업 꺼짐
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+                isTopFocus = false;
+
+            // esc - 설정창 끄기
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Debug.Log("설정 팝업 닫힘");
+                this.popUp.SetActive(false); // 팝업 꺼짐
+            }
         }
-        DontDestroyOnLoad(this); // 씬이 넘어가도 이 스크립트는 파괴되면 안됨
+
+        // esc - 상위로 (오디오 버튼으로)
+        else
+        {
+            //ChangeTextColor(currentBtnText);
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                EventSystem.current.SetSelectedGameObject(currentButton);
+                isTopFocus = true;
+            }
+        }
+
+        //DontDestroyOnLoad(this); // 씬이 넘어가도 이 스크립트는 파괴되면 안됨
     }
 
 
+    // 선택된 버튼 색상 흰색으로 바꾸는 함수
+    public void ChangeTextColor()
+    {
+        foreach (var btnText in topButtonTexts)
+        {
+            if (btnText == currentBtnText)
+                btnText.color = selectedColor;   // 선택된 버튼 흰색
+            else
+                btnText.color = defaultColor;   // 나머지 보라색
+        }
+    }
+
     public void OnClickAudioButton()
     {
+        currentBtnText = topButtonTexts[0]; // 오디오 버튼 텍스트로 현재 버튼 텍스트 설정
         audioPopup.SetActive(true);          //audio tab active, others inactive
         graphicPopup.SetActive(false);
         controlPopup.SetActive(false);
         Debug.Log("오디오 설정창 활성화");
-
-
     }
 
     public void OnClickGraphicButton()
     {
+        currentBtnText = topButtonTexts[1]; // 그래픽 버튼 텍스트로 현재 버튼 텍스트 설정
         graphicPopup.SetActive(true);       //graphic tab active, others inactive
         audioPopup.SetActive(false);
         controlPopup.SetActive(false);
@@ -76,6 +118,7 @@ public class SettingsPopup : UIBase
 
     public void OnClickControlButton()
     {
+        currentBtnText = topButtonTexts[2]; // 조작법 버튼 텍스트로 현재 버튼 텍스트 설정
         controlPopup.SetActive(true);       //control tab active, others inactive
         graphicPopup.SetActive(false);
         audioPopup.SetActive(false);
