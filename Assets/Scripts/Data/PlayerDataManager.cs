937b3b5f624cb35c07637b4a4fe6a9e7f2b18235
조수민 <josoomin6954@naver.com>
2025-08-13 16:11:51 +0900

    체력 100으로 수정. Heal(), TakeDamage() 버그픽스
    Heal() 작동시 30% 확률로 한칸 추가적으로 회복됨

--- Assets/Scripts/Data/PlayerDataManager.cs
+++ Assets/Scripts/Data/PlayerDataManager.cs
@@ -26,8 +26,8 @@ public class PlayerData
 
     public PlayerData()
     {
-        hp = 10;
-        maxHp = 10;
+        hp = 100;  //한칸이 20 반칸이 10 풀피가 100
+        maxHp = 100;
         damage = 10f;
         addDamage = 0f;
         itemDropRate = 1f;
--- Assets/Scripts/HpUIManager.cs
+++ Assets/Scripts/HpUIManager.cs
@@ -7,7 +7,7 @@ using UnityEngine.Rendering;
 // 인게임, 인벤토리, 세이브파일화면에서 쓸려고 만듦
 // 체력 하트 UI 관리
 
-public class HpManager : MonoBehaviour
+public class HpUIManager : MonoBehaviour
 {
     public GameObject[] hearts; // 채워진 하트 이미지들
     private int maxHp;
@@ -29,16 +29,16 @@ public class HpManager : MonoBehaviour
     //현재 hp를 ui에 하트로 표시
     public void UpdateHearts(int currentHp)
     {
-        int hp = currentHp; 
+        int hp = currentHp/10; 
 
         for (int i = 0; i < hearts.Length; i++)
         {
-            if (i <= hp)
+            if (i < hp)
                 hearts[i].SetActive(true); // 하트 보이기
             else
                 hearts[i].SetActive(false); // 하트 숨기기
         }
-        Debug.Log("현재 HP = " + hp); //임시 확인용
+        Debug.Log("현재 HP = " + hp*10); //임시 확인용
     }
 
     // 체력 회복 함수(물약 사용)
@@ -46,10 +46,13 @@ public class HpManager : MonoBehaviour
     {
         int currentHp = PlayerDataManager.Instance.GetHp(); // 현재 체력 가져오기
         int additionalHealingRate = PlayerDataManager.Instance.GetAdditionalHealingAmount(); // 추가 회복율 가져오기 //////////////보류
+        PlayerDataManager.Instance.SetHp(healingAmout); // 체력을 한칸 증가
 
-        PlayerDataManager.Instance.SetHp(healingAmout + additionalHealingRate); // 체력을 1 * (1+추가회복비율) 증가  ////////////////보류
+        if (UnityEngine.Random.value < 0.3f)        // 30% 확률로 한칸 추가 회복
+            PlayerDataManager.Instance.SetHp(1);
+
+        Debug.Log($"HP +{healingAmout * (1 + additionalHealingRate)}");
         UpdateHearts(PlayerDataManager.Instance.GetHp());
-        Debug.Log($"HP +{healingAmout * (1 + additionalHealingRate)}"); 
     }
 
     // 체력 최대로 회복 함수(특정 지점에 가면)
@@ -57,8 +60,8 @@ public class HpManager : MonoBehaviour
     {
         int currentHp = maxHp; // 최대 체력으로 설정
         PlayerDataManager.Instance.SetHp(currentHp); // maxHp를 현재 체력에 더해서 max로 만듦
-        UpdateHearts(PlayerDataManager.Instance.GetHp()); 
         Debug.Log("HP Full");
+        UpdateHearts(PlayerDataManager.Instance.GetHp()); 
     }
 
 
@@ -67,8 +70,8 @@ public class HpManager : MonoBehaviour
     {
         int currentHp = PlayerDataManager.Instance.GetHp(); // 현재 체력 가져오기
         PlayerDataManager.Instance.SetHp(-damage); // 현재 체력에 damage를 뺌
-        UpdateHearts(PlayerDataManager.Instance.GetHp());
         Debug.Log($"HP -{damage}");
+        UpdateHearts(PlayerDataManager.Instance.GetHp());
     }
 
     // 최대 체력 증가
