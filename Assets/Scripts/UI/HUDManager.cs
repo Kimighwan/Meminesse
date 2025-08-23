41ac6588fab5296b05f7d83efd225dd66be96b17
조수민 <josoomin6954@naver.com>
2025-08-23 19:26:36 +0900

    일시정지창 (설정 빼고)

--- Assets/Scripts/UI/HUDManager.cs
+++ Assets/Scripts/UI/HUDManager.cs
@@ -8,7 +8,9 @@ public class HUDManager : MonoBehaviour
     private GameObject pauseMenuPopUp;
 
     [SerializeField]
-    private GameObject exitConfirmationPopupUI;   // 키보드 모드 오류 해결용
+    private GameObject GoToMainConfirmationPopupUI; // 진행중인 게임을 종료하고 메인 메뉴로 나가시겠습니까?
+    [SerializeField]
+    private GameObject exitConfirmationPopupUI;   // 종료하시겠습니까? 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,16 +23,25 @@ public class HUDManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            pauseMenuPopUp.SetActive(true);
-            // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 게임 멈추기
+            bool isActive = pauseMenuPopUp.activeSelf;
+            pauseMenuPopUp.SetActive(!isActive);
 
-            if (Input.GetKeyDown(KeyCode.Escape)) OnClickResume();
+            if (!isActive)
+            {
+                // 메뉴를 켤 때
+                // 게임 멈춤 로직 작성 부분
+            }
+            else
+            {
+                // 메뉴를 끌 때
+                // 게임 재개 로직 작성 부분
+            }
         }
     }
     public void OnClickResume()
     {
         pauseMenuPopUp.SetActive(false);
-        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 게임 다시 진행
+        // 게임 재개 로직 작성 부분
     }
     public void OnClickSetting()
     {
@@ -38,13 +49,48 @@ public class HUDManager : MonoBehaviour
     }
     public void OnClickMainMenu()
     {
-        SceneManager.LoadScene("MainMenu");
+        pauseMenuPopUp.SetActive(false);
+        GoToMainConfirmationPopupUI.SetActive(true);
     }
     public void OnClickQuit()
     {
-        if (pauseMenuPopUp.activeSelf) return;
+        //if (pauseMenuPopUp.activeSelf) return;
+        pauseMenuPopUp.SetActive(false);
         exitConfirmationPopupUI.SetActive(true);
         //SetCurrentButton(exitConfirmationPopupUI.transform.Find("Yes").gameObject); // 팝업의 Yes 버튼에 포커스 설정
 
     }
+
+    // 진행중인 게임을 종료하고 메인 메뉴로 나가시겠습니까?
+    public void OnConfirmGoToMainMenu() // 예
+    {
+        
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void OnCancelGoToMainMenu() // 아니오
+    {
+        pauseMenuPopUp.SetActive(true);
+        GoToMainConfirmationPopupUI.SetActive(false);
+    }
+
+
+    // 종료하시겠습니까?
+    public void OnConfirmQuit() // 예
+    {
+        Debug.Log("Game closed");
+#if UNITY_EDITOR                  //Unity 에디터에서 실행 중일 때만 아래 코드를 실행
+        UnityEditor.EditorApplication.isPlaying = false;       //에디터에서 Play 중지
+#else
+        Application.Quit();       //실제 게임에서 종료
+#endif
+    }
+    public void OnCancelQuit() // 아니오
+    {
+        // 팝업 닫기
+        exitConfirmationPopupUI.SetActive(false);
+        pauseMenuPopUp.SetActive(true);
+        // 창이 넘어갔을 때 포커스에 메인에 그대로 있는 문제 해결
+        if (exitConfirmationPopupUI.activeSelf) return;
+    }
+
 }
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -47,5 +47,25 @@ public class MainMenu : UIBase
 
     }
 
+    // 종료하시겠습니까 - 예
+    public void OnConfirmQuit()
+    {
+        Debug.Log("Game closed");
+#if UNITY_EDITOR                  //Unity 에디터에서 실행 중일 때만 아래 코드를 실행
+        UnityEditor.EditorApplication.isPlaying = false;       //에디터에서 Play 중지
+#else
+        Application.Quit();       //실제 게임에서 종료
+#endif
+    }
+
+    // 종료하시겠습니까 - 아니오
+    public void OnCancelQuit()
+    {
+        // 팝업 닫기
+        exitConfirmationPopupUI.SetActive(false);
+
+        // 창이 넘어갔을 때 포커스에 메인에 그대로 있는 문제 해결
+        if (exitConfirmationPopupUI.activeSelf) return;
+    }
 
 }
