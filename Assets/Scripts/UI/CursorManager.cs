7976e77b58e0c8caa7033cd37d63ec1c4d18cf38
조수민 <josoomin6954@naver.com>
2025-11-16 17:23:22 +0900

    fix: 커서 이미지 교체, 크기 작게

--- Assets/Scripts/UI/CursorManager.cs
+++ Assets/Scripts/UI/CursorManager.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class CursorManager : MonoBehaviour
 {
     public Texture2D customCursor;
-    public Vector2 hotspot = Vector2.zero; // 커서 기준점 (왼쪽 위면 0,0 / 가운데면 이미지의 절반)
+    public Vector2 hotspot = Vector2.zero;
 
     void Start()
     {
-        Cursor.SetCursor(customCursor, hotspot, CursorMode.Auto);   //커스텀 이미지사용, 커서 이미지 안에서 실제로 클릭되는 지점, 기본 모드
+        Cursor.SetCursor(customCursor, hotspot, CursorMode.ForceSoftware);
     }
 }
